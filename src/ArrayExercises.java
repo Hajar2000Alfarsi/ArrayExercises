2efeb3f2b5f38ab0f7c22b56cbebac12f11b5707
Hajar ALfarsi <hajar2000alfarsi@gamil.com>
2026-04-06 10:31:19 +0400

    task1 answer of firstLast6

--- src/ArrayExercises.java
+++ src/ArrayExercises.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
-
 public class ArrayExercises {
+    public static void main(String[] args){
+
+        //Task 1: firstLast6
+        int[] firstArray= {1,2,6};
+        System.out.println(firstLast6(firstArray));
+        int[] scoandArray= {1,2,6,5,8};
+        System.out.println(firstLast6(scoandArray));
+    }
+
+    //Task 1: firstLast6
+    public static boolean firstLast6(int nums[]){
+        if(nums[0]==6 || nums[nums.length-1]==6){
+            return true;
+        }else
+        {
+            return false;
+        }
+    }
 }
